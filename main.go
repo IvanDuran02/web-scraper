0d5c799e9592409dfede9ef1b1497c279ab85105
Ivan Duran <iamivanduran02@gmail.com>
2024-03-21 16:36:14 -0400

    made it so the quality links are straight to download, now we can clean the code up a bit

--- main.go
+++ main.go
@@ -11,6 +11,42 @@ type WallpaperStruct struct {
 	title, url, HD_1080p, HD_1440p, HD_2160p string
 }
 
+func getDownload(link string) string {
+	// Instantiate a new Collector
+	c := colly.NewCollector()
+
+	// Create a channel to communicate the found link
+	linkChan := make(chan string, 1) // Buffered channel with a capacity of 1
+
+	c.OnHTML("img.d_img_holder", func(e *colly.HTMLElement) {
+		downloadLink := e.Attr("src")
+		fmt.Println("Checking: " + link + "\n\tGot: \n\t\t" + downloadLink)
+		linkChan <- downloadLink // Send the found link back through the channel
+	})
+
+	c.Visit(link)
+
+	// Wait to receive the link from the channel
+	result := <-linkChan
+	close(linkChan) // Close the channel
+
+	return result
+}
+
+func removeDuplicates(wallpapers []WallpaperStruct) []WallpaperStruct {
+	seen := make(map[string]bool)
+	var uniqueWallpapers []WallpaperStruct
+
+	for _, wallpaper := range wallpapers {
+		if _, ok := seen[wallpaper.url]; !ok {
+			seen[wallpaper.url] = true
+			uniqueWallpapers = append(uniqueWallpapers, wallpaper)
+		}
+	}
+
+	return uniqueWallpapers
+}
+
 func main() {
 	// Instantiate a new Collector
 	c := colly.NewCollector()
@@ -18,6 +54,7 @@ func main() {
 	// Define a slice to store the wallpapers
 	var wallpapers []WallpaperStruct
 	urlTemplate := "https://hdqwalls.com/%s"
+
 	// OnHTML callback for 'a' elements
 	c.OnHTML("a", func(e *colly.HTMLElement) {
 		// Extract the title attribute from the 'a' element
@@ -39,9 +76,9 @@ func main() {
 			wallpapers = append(wallpapers, WallpaperStruct{
 				title:    title,
 				url:      modifiedURL,
-				HD_1080p: "https://hdqwalls.com/wallpaper/1920x1080/" + modifiedTitle,
-				HD_1440p: "https://hdqwalls.com/wallpaper/2560x1440/" + modifiedTitle,
-				HD_2160p: "https://hdqwalls.com/wallpaper/3840x2160/" + modifiedTitle,
+				HD_1080p: getDownload("https://hdqwalls.com/wallpaper/1920x1080/" + modifiedTitle),
+				HD_1440p: getDownload("https://hdqwalls.com/wallpaper/2560x1440/" + modifiedTitle),
+				HD_2160p: getDownload("https://hdqwalls.com/wallpaper/3840x2160/" + modifiedTitle),
 			})
 		}
 	})
@@ -52,6 +89,9 @@ func main() {
 		fmt.Println("Error visiting the website:", err)
 		return
 	}
+
+	// Filter duplicates based on URL
+	wallpapers = removeDuplicates(wallpapers)
 
 	// Print the wallpapers
 	fmt.Println("Wallpapers:")
